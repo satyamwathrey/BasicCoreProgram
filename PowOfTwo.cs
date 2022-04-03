fda00bdbf8d25184c1a9a659e587b70c60a0ba08
satyamwathrey <swathrey1@gmail.com>
2022-04-03 12:24:36 +0530

    Power of two of a given number is shown

---
+++ PowOfTwo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+
+            Console.Write("Enter a number:");//input from User
+            num = Convert.ToInt32(Console.ReadLine());
+            int result = num * num; //num*2 if 2*2==4
+            Console.WriteLine(result);//Op
+            Console.ReadLine();
+        }
+    }
+}
