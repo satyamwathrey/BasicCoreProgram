a7c042147c4e65e20f469d335714ebf4e25e5297
satyamwathrey <swathrey1@gmail.com>
2022-04-03 12:44:48 +0530

    Swaps the given two digit number

---
+++ SwapNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first, second, temp = 0;
+
+            Console.WriteLine("Enter First Number :- ");
+            first = Convert.ToInt32(Console.ReadLine()); //"10"
+
+            Console.WriteLine("Enter Second Number :- ");
+            second = Convert.ToInt32(Console.ReadLine()); //"20"
+
+            Console.WriteLine("Before Swapping :");
+            Console.WriteLine("First :- {0} Second :- {1}", first, second);
+            temp = first; // temp=10;
+            first = second; //first=20;
+            second = temp; //second=10;
+
+            Console.WriteLine("After Swapping :");
+            Console.WriteLine("First :- {0} Second :- {1}", first, second);
+        }
+    }
+}
