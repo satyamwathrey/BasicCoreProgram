ed85bb6f8b017ad58c098112f57bb322932a473f
satyamwathrey <swathrey1@gmail.com>
2022-04-03 12:00:18 +0530

    Checks leap year from given year

---
+++ LeapYear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter to Year(YYYY) to Check if it is Leap Year or Not: ");//Input From User
+            int Checkyear = Convert.ToInt32(Console.ReadLine()); //ConvertToInt
+
+            if ((Checkyear % 400 == 0) || (Checkyear % 4 == 0 && Checkyear % 100 != 0))//checking if year is % of 400 or %4 then its a leap year if %100 its not a leap year
+                Console.WriteLine(Checkyear + " is a Leap Year"); //For leap Year
+            else
+                Console.WriteLine(Checkyear + " is not a Leap Year"); //For Not a Leap Year
+
+
+        }
+    }
+}
