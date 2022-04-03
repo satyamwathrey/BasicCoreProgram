e83e22af67033f04b7de21a23cca157cbf8ad267
satyamwathrey <swathrey1@gmail.com>
2022-04-03 12:41:09 +0530

    Gives the Remainder and Quotient of given Dividend and Divisor

---
+++ QuotientRemainder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Dividend = 0;
+            int Divisor = 0;
+
+            Console.Write("Calculate the Dividend:"); //Input From Dividend
+            Dividend = Convert.ToInt32(Console.ReadLine());//reading Dividend input from user
+            Console.Write("Calculate the Divisor:"); //Input From Divisor
+            Divisor = Convert.ToInt32(Console.ReadLine());//reading Divisor input from user
+            int AndQuotient = Dividend / Divisor;  //calculate example (5/2)
+            int AndRemainder = Dividend % Divisor; // calculate example (5%2)
+            Console.WriteLine("Dividend:{0} Divisor:{1}", Dividend, Divisor); //redaing op
+            Console.WriteLine("Quotient = " + AndQuotient); //op for Quotient
+            Console.WriteLine("Remainder = " + AndRemainder); //Op for Remainder
+            Console.ReadLine();
+        }
+    }
+}
